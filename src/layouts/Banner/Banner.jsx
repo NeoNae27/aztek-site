7c93e5d2f3370e4581bb599a3b5d272f392cf640
Nick <nikitaromanov60@gmail.com>
2025-05-21 12:06:38 +0300

    Banner Rework

--- src/layouts/Banner/Banner.jsx
+++ src/layouts/Banner/Banner.jsx
@@ -3,15 +3,22 @@ import { Button } from "@components";
 
 const Section = () => {
   return (
-    <section>
-      <div className="content">
-        <div className="headliner">
-          <h2>
+    <section className="banner">
+      <div className="banner__content">
+        <div className="banner__headliner">
+          <h2 className="banner__title">
             Where Engineering <br /> Meets Enterprise
           </h2>
-          <h4>Powering Industry Since 2004</h4>
+          <h4 className="banner__subtitle">Powering Industry Since 2004</h4>
         </div>
-        <Button label="OUR PROJECTS" size="button-lg" type="outline" />
+        <div className="banner__buttons">
+          <Button
+            label="OUR PROJECTS"
+            size="button-lg"
+            type="outline"
+            aria-label="View our projects"
+          />
+        </div>
       </div>
     </section>
   );

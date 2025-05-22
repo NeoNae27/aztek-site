0be44ec8093519d355fd7a2efb5c08fea26cdd8e
Nick <nikitaromanov60@gmail.com>
2025-05-22 22:51:40 +0300

    Create variables in styles.scss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ filename: 'dist/stats.html', open: true })],
   server: {
     watch: {
       usePolling: true,

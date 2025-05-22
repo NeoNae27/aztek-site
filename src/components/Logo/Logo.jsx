2e41b75fd6e18a376fc5f24bf82eff93ce68ec22
Nick <nikitaromanov60@gmail.com>
2025-05-22 15:54:09 +0300

    Imgs Fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import "./Logo.scss";
 const Logo = () => {
   return (
      <span className="logo">
-      <img src="/aztek-white-logo.svg" alt="Logo" width="24" height="24" />
+      <img src="./aztek-white-logo.svg" alt="Logo" width="24" height="24" />
       <a href="#home" className="subtitle2">
         AZTEK
       </a>
